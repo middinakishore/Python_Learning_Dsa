3b7a2dd9f6915129eda130d1689d324748444327
DevKishore <middinakishoreyada@gmail.com>
2026-07-20 12:06:47 +0530

    Day-1 Learning

---
+++ P1.py
@@ -0,0 +1,27 @@
+'''for i in range(3):
+    print("Hello World")
+'''
+'''n = int(input("enter number:"))
+if n%2 == 0:
+    print("Even")
+else:
+    print("odd")'''
+'''def count(*args):
+    print(type(args))
+count(1,2,3,4,5)'''
+'''x = "kishore"
+print(x[1:6])'''
+'''while True:
+    print("hi")'''
+'''n= int(input("enter number:"))
+x = 0
+for i in range(n+1):
+    x += i
+print(x)'''
+n= int(input("enter number:"))
+x = 0
+i = 0
+while i <= n:
+    x +=i
+    i +=1
+print(x)
